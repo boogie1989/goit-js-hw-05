03f32861878bc4d984f00591cc4dcf90fbf31964
Serhii Buhai <serhiibuhai.work@gmail.com>
2024-09-04 13:33:32 +0300

    review fixes

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = users =>
-  users.sort((a, b) => b.friends.length - a.friends.length);
+  Array.from(users).sort((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
